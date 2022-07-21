b1b479b069bb972379625d12d0eb4f3582ed36ca
vsheva <valera.sheva@gmail.com>
2022-07-22 01:41:56 +0300

    add class-based component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -30,3 +30,49 @@ const Counter = () => {
 };
 
 export default Counter;
+
+
+/**
+//Class-based component
+
+import classes from './Counter.module.css';
+import {connect} from 'react-redux';
+import {Component} from "react";
+
+class Counter extends Component {
+
+    handleIncrement = () => {this.props.increment()}; //вызов increment:()=>dispatch({type: 'increment'})
+
+    handleDecrement = () => {this.props.decrement()};
+
+    toggleCounterHandler = () => {};
+
+    render() {
+        return (
+            <main className={classes.counter}>
+                <h1>Redux Counter</h1>
+                <div className={classes.value}>{this.props.counter}</div>
+                <div>
+                    <button onClick={this.handleIncrement}>Increment</button>
+                    <button onClick={this.handleDecrement}>Decrement</button>
+                </div>
+                <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+            </main>
+        );
+    }
+};
+
+const mapStateToProps = (state)=>{
+    return {
+        counter: state.counter
+    }
+};
+
+const mapDispatchToProps = (dispatch)=>{
+    return {
+        increment:()=>dispatch({type: 'increment'}),
+        decrement:()=>dispatch({type: 'decrement'}),
+    }
+}
+
+export default connect (mapStateToProps,mapDispatchToProps) (Counter);*/
